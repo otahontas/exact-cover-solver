a5f8896c1b136935430001c7e8f5296e8737c386
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-04 22:52:34 +0200

    Add solver service

---
+++ src/services/solver.py
@@ -0,0 +1,10 @@
+"""Solver service."""
+
+
+class Solver:
+    """Service for solving an exact cover problem from given input."""
+
+    @staticmethod
+    def solve(universe, set_collection):
+        pass
+
