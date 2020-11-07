14fb53adbfd7b630da763b21f0ab17974c429f52
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-07 20:28:44 +0200

    Move dancing links matrix creation inside dlxmatrix class

--- src/datastructures/dlxmatrix.py
+++ src/datastructures/dlxmatrix.py
@@ -1,14 +1,60 @@
 """DLX matrix implementation based on data objects."""
 
+from .dlxdataobjects import DataObject, ColumnObject
+
+
 class DLXMatrix:
-    pass
+    def __init__(self, universe, set_collection):
+        """Create column and data objects and link them to this matrix object."""
+        self.create_columns(universe)
+        self.create_nodes(set_collection)
 
-class RootNode:
-    """Root node which links header columns together."""
+    def create_columns(self, universe):
+        """Create column columns and attach them to root."""
+        previous = self
+        for element in universe:
+            column = ColumnObject(_id=element)
+            column.left = previous
+            previous.right = column
+            previous = column
+        self.left = previous
+        previous.right = self
 
-    def __init__(self, left=None, right=None):
-        """Initialize root."""
-        self.left = left
-        self.right = right
-        self.id = "Root"
+    def create_nodes(self, root, set_collection):
+        """Create nodes representing elements in each set in set collection.
 
+        All sets are iterated through. For each element in set the element is
+        linked to the correct column and columns column. Elements in set are
+        also linked together to form a circular row.
+        """
+
+        def find_column(_id):
+            column = root
+            while column.id != _id:
+                column = column.right
+            return column
+
+        for set_name, set_elements in set_collection:
+            previous = None
+            first = None
+            for element in set_elements:
+                column = find_column(element)
+                cell = DataObject(column, row=set_name)
+                if not column.up and not column.down:
+                    column.up = column.down = cell
+                    cell.up = cell.down = column
+                else:
+                    prev_up = column.up
+                    prev_up.down = cell
+                    cell.up = prev_up
+                    cell.down = column
+                    column.up = cell
+                if previous:
+                    previous.right = cell
+                    cell.left = previous
+                else:
+                    first = cell
+                previous = cell
+                column.increase_count()
+            previous.right = first
+            first.left = previous
--- src/services/solver.py
+++ src/services/solver.py
@@ -1,7 +1,7 @@
 """Solver service, handles different solving modes."""
 
-from datastructures.node import Node, HeaderNode, RootNode
-from algos.algorithmX import AlgorithmX
+from datastructures.dlxmatrix import DLXMatrix
+from algos.dlx import DLX
 
 
 class Solver:
@@ -12,63 +12,7 @@ class Solver:
 
         Input should be universe of elements (such as numbers from 1 to seven)
         and collection of sets consisting elements from universe."""
-        root = self.create_root()
-        self.create_headers(root, universe)
-        self.create_nodes(root, set_collection)
-        algoX = AlgorithmX()
-        algoX.solve(root)
+        matrix = DLXMatrix(universe, set_collection)
+        dlx = DLX()
+        dlx.solve(matrix)
 
-    def create_root(self):
-        """Create root column."""
-        return RootNode()
-
-    def create_headers(self, root, universe):
-        """Create header columns and attach them to root."""
-        previous = root
-        for element in universe:
-            header = HeaderNode(element)
-            header.left = previous
-            previous.right = header
-            previous = header
-        root.left = previous
-        previous.right = root
-
-    def create_nodes(self, root, set_collection):
-        """Create nodes representing elements in each set in set collection.
-
-
-        All sets are iterated through. For each element in set the element is
-        linked to the correct column and columns header. Elements in set are
-        also linked together to form a circular row.
-        """
-
-        def find_header(header_id):
-            header = root
-            while header.id != header_id:
-                header = header.right
-            return header
-
-        for set_name, set_elements in set_collection:
-            previous = None
-            first = None
-            for element in set_elements:
-                header = find_header(element)
-                cell = Node(header, set_name)
-                if not header.up and not header.down:
-                    header.up = header.down = cell
-                    cell.up = cell.down = header
-                else:
-                    prev_up = header.up
-                    prev_up.down = cell
-                    cell.up = prev_up
-                    cell.down = header
-                    header.up = cell
-                if previous:
-                    previous.right = cell
-                    cell.left = previous
-                else:
-                    first = cell
-                previous = cell
-                header.increase_count()
-            previous.right = first
-            first.left = previous
