a42cf43343a70a19d7ab33d944779a3c8bb22d33
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-07 18:37:39 +0200

    Remove extra code from main.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,9 @@
-from services.solver import Solver
+"""Main entrypoint for program."""
 
 
-def main():
-    set_collection = [
-        ("A", [1, 4, 7]),
-        ("B", [1, 4]),
-        ("C", [4, 5, 7]),
-        ("D", [3, 5, 6]),
-        ("E", [2, 3, 6, 7]),
-        ("F", [2, 7]),
-    ]
-    universe = [i for i in range(1, 8)]
-    solver = Solver()
-    solver.solve(universe, set_collection)
+def main() -> None:
+    """Launch UI from main, not implemented yet."""
+    raise NotImplementedError
 
 
 if __name__ == "__main__":
