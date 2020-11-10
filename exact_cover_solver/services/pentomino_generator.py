f89ba947d2dd627f62c6e3e47e855a11db078a4b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-10 14:37:54 +0200

    Fix some typos

--- exact_cover_solver/services/pentomino_generator.py
+++ exact_cover_solver/services/pentomino_generator.py
@@ -1,6 +1,6 @@
 """Service for generating universe and sets for pentomino problems."""
 
-NUMBER_OF_PENTOMINOES = 12
+NUMBER_OF_PENTOMINOES = BASE = 12
 NUMBER_OF_CELLS = 60
 
 
@@ -14,6 +14,7 @@ class PentominoGenerator:
         self.universe = [i for i in range(NUMBER_OF_PENTOMINOES + NUMBER_OF_CELLS)]
         self.set_collection = []
         # Different pentominoes as two-dimensional arrays
+        # Order: V, U, X, T, Y, I, F, P, W, Z, N, L
         self.pentominoes = [
             [[1, 1, 1], [1, 0, 0], [1, 0, 0]],
             [[1, 0, 1], [1, 1, 1]],
@@ -33,21 +34,21 @@ class PentominoGenerator:
         """Generate universe and set collection for algo x."""
         pass
 
-    def generate_all_orientations(self, pentomino, pentomino_index):
+    def generate_all_orientations(self, pentomino):
         """Generate different orientations for this pentomino."""
         seen = set()
         orientations = []
         for transposed in (pentomino, self.transpose(pentomino)):
             for left_right_flipped in (transposed, self.flip_left_right(transposed)):
                 for up_down_flipped in (
-                    left_right_flipped,
-                    self.flip_up_down(left_right_flipped),
+                        left_right_flipped,
+                        self.flip_up_down(left_right_flipped),
                 ):
                     orientation_as_string = str(up_down_flipped)
                     if orientation_as_string not in seen:
                         seen.add(orientation_as_string)
                         orientations.append(up_down_flipped)
-        if pentomino_index == 10:
+        if pentomino == self.pentominoes[10]:
             # Skip duplicates for pentomino number 10
             return orientations[::2]
         return orientations
