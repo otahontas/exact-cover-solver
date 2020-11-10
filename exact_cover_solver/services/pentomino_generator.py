c8b036fc91db8fb23846e82aa7a5b22a3744d392
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-10 14:11:30 +0200

    Add docstring and fix up down flipping

--- exact_cover_solver/services/pentomino_generator.py
+++ exact_cover_solver/services/pentomino_generator.py
@@ -8,6 +8,7 @@ class PentominoGenerator:
     """Universe and set generator for different size pentominoes."""
 
     def __init__(self, height: int, width: int) -> None:
+        """Initialize generator."""
         self.height = height
         self.width = width
         self.universe = [i for i in range(NUMBER_OF_PENTOMINOES + NUMBER_OF_CELLS)]
@@ -25,18 +26,23 @@ class PentominoGenerator:
             [[1, 1, 0], [0, 1, 1], [0, 0, 1]],
             [[1, 0, 0], [1, 1, 1], [0, 0, 1]],
             [[1, 1, 0, 0], [0, 1, 1, 1]],
-            [[1, 1, 1, 1], [1, 0, 0, 0]]
+            [[1, 1, 1, 1], [1, 0, 0, 0]],
         ]
 
     def generate(self):
+        """Generate universe and set collection for algo x."""
         pass
 
     def generate_all_orientations(self, pentomino, pentomino_index):
+        """Generate different orientations for this pentomino."""
         seen = set()
         orientations = []
         for transposed in (pentomino, self.transpose(pentomino)):
             for left_right_flipped in (transposed, self.flip_left_right(transposed)):
-                for up_down_flipped in (left_right_flipped, self.flip_up_down(left_right_flipped)):
+                for up_down_flipped in (
+                    left_right_flipped,
+                    self.flip_up_down(left_right_flipped),
+                ):
                     orientation_as_string = str(up_down_flipped)
                     if orientation_as_string not in seen:
                         seen.add(orientation_as_string)
@@ -46,13 +52,17 @@ class PentominoGenerator:
             return orientations[::2]
         return orientations
 
-    def flip_up_down(self, pentomino):
-        return self.transpose(self.flip_left_right(self.transpose(pentomino)))
+    @staticmethod
+    def flip_up_down(pentomino):
+        """Return pentomino with rows reversed."""
+        return pentomino[::-1]
 
     @staticmethod
     def transpose(pentomino):
+        """Return pentomino with rows and columsns transposed."""
         return [list(x) for x in zip(*pentomino)]
 
     @staticmethod
     def flip_left_right(pentomino):
+        """Return pentomino with each row reversed."""
         return [row[::-1] for row in pentomino]
