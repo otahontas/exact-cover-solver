0419928e262905eb2f0d9da154d7dd26c83ae2d9
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-20 17:57:49 +0200

    Add time reporting to performance tester

--- performance_tests/test_solver.py
+++ performance_tests/test_solver.py
@@ -3,6 +3,7 @@
 from exact_cover_solver.services.pentomino_generator import PentominoGenerator
 from exact_cover_solver.datastructures.dlxmatrix import DLXMatrix
 from exact_cover_solver.algos.dlx import DLX
+import time
 
 
 def test_pentomino_board_gets_correct_amount_of_solutions():
@@ -11,5 +12,10 @@ def test_pentomino_board_gets_correct_amount_of_solutions():
     universe, set_collection = pg.generate()
     matrix = DLXMatrix(universe, set_collection)
     dlx = DLX()
+    start_time = time.time()
     dlx.solve(matrix)
-    assert len(dlx.solutions) == 2339
+    time_processing = time.time() - start_time
+    print(f"Found {len(dlx.solutions)} solutions in {time_processing} seconds")
+
+
+test_pentomino_board_gets_correct_amount_of_solutions()
