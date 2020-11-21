fbba5523276c34c41900d5e3d8c3def13cdc9404
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-21 02:51:40 +0200

    Fix tests for dlx data structures

--- tests/datastructures/test_dlxdataobjects.py
+++ tests/datastructures/test_dlxdataobjects.py
@@ -4,35 +4,28 @@ from exact_cover_solver.datastructures.dlxdataobjects import DataObject, ColumnO
 
 
 def test_column_objects_column_is_linked_to_self_on_init():
-    column = ColumnObject()
+    column = ColumnObject(1)
     assert column.column == column
 
 
-def test_column_object_has_no_size_and_id_on_init_without_args():
-    column = ColumnObject()
-    assert column.id is None
+def test_column_object_has_no_size_on_init():
+    column = ColumnObject(1)
     assert column.size == 0
 
 
 def test_column_object_gets_id_from_args():
-    column = ColumnObject("A")
-    assert column.id == "A"
+    column = ColumnObject(2)
+    assert column.id == 2
 
 
 def test_data_object_is_linked_to_self_on_init():
-    column = ColumnObject()
-    data = DataObject(column)
+    column = ColumnObject(1)
+    data = DataObject(column, "1")
     assert data.up is data.down is data.left is data.right is data
     assert data.column is column
 
 
-def test_data_object_has_no_row_on_init():
-    column = ColumnObject()
-    data = DataObject(column)
-    assert data.row is None
-
-
 def test_data_object_gets_row_from_args():
-    column = ColumnObject()
+    column = ColumnObject(1)
     data = DataObject(column, "1")
     assert data.row == "1"
