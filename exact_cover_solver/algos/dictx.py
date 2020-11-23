38c25820d4e40da491a064b3089c70b4d6ed2f0c
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-23 21:16:36 +0200

    Add dictx base

---
+++ exact_cover_solver/algos/dictx.py
@@ -0,0 +1,13 @@
+"""Dancing links implementation for algorithm X."""
+
+from typing import List, Any
+
+from exact_cover_solver.algos import AlgorithmX, Solution
+
+
+class DictX(AlgorithmX):
+    """Dictionary based implementation for algorithm X."""
+
+    def solve(self, matrix: Any) -> List[Solution]:
+        """Not implemented."""
+        raise NotImplementedError
