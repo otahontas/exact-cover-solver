45eef6d22ae48447d9c2674c32894b61c968cc2b
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-24 16:01:09 +0200

    Fix performance test timers

--- performance_tests/test_pentominoes_with_dlx.py
+++ performance_tests/test_pentominoes_with_dlx.py
@@ -5,6 +5,7 @@ https://en.wikipedia.org/wiki/Pentomino#Constructing_rectangular_dimensions
 """
 
 from exact_cover_solver.services.solver import Solver
+import time
 
 
 def generate_pentomino_board_solutions(
@@ -13,15 +14,14 @@ def generate_pentomino_board_solutions(
     """Test that 6x10 board gets correct amount of solutions."""
     solver = Solver()
     solver.algorithm = "DLX"
+    start_time = time.time()
     browser = solver.solve_pentomino_problem(board_height, board_width)
     solutions = browser._solutions
+    time_solving = time.time() - start_time
     print(
         f"There should be {correct_amount} solutions for {board_height}x{board_width} "
-        f"board. Algorithm found {len(solutions)} solutions"
+        f"board. Algorithm found {len(solutions)} solutions in {time_solving} secs."
     )
-    print("first board:")
-    for row in browser.current_board:
-        print(row)
 
 
 generate_pentomino_board_solutions(3, 20, 2)
