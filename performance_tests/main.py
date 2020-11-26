de627ab1bb7115644b43afa7a0f57db5c6705586
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-11-26 13:07:14 +0200

    Add dictx performance tests and check performance tests to run only with pypy

--- performance_tests/main.py
+++ performance_tests/main.py
@@ -9,11 +9,11 @@ import time
 
 
 def generate_pentomino_board_solutions(
-    board_height: int, board_width: int, correct_amount: int
+    algo, board_height: int, board_width: int, correct_amount: int
 ) -> None:
     """Test that 6x10 board gets correct amount of solutions."""
     solver = Solver()
-    solver.algorithm = "DLX"
+    solver.algorithm = algo
     start_time = time.time()
     browser = solver.solve_pentomino_problem(board_height, board_width)
     solutions = browser._solutions
@@ -24,7 +24,12 @@ def generate_pentomino_board_solutions(
     )
 
 
-generate_pentomino_board_solutions(3, 20, 2)
-generate_pentomino_board_solutions(4, 15, 368)
-generate_pentomino_board_solutions(5, 12, 1010)
-generate_pentomino_board_solutions(6, 10, 2339)
+generate_pentomino_board_solutions("DLX", 3, 20, 2)
+generate_pentomino_board_solutions("DLX", 4, 15, 368)
+generate_pentomino_board_solutions("DLX", 5, 12, 1010)
+generate_pentomino_board_solutions("DLX", 6, 10, 2339)
+
+generate_pentomino_board_solutions("DictX", 3, 20, 2)
+generate_pentomino_board_solutions("DictX", 4, 15, 368)
+generate_pentomino_board_solutions("DictX", 5, 12, 1010)
+generate_pentomino_board_solutions("DictX", 6, 10, 2339)
