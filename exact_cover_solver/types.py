17a49ad61b02a4422e8a647c2e03de70bf7a713c
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-12-10 18:11:07 +0200

    Modify some types

--- exact_cover_solver/types.py
+++ exact_cover_solver/types.py
@@ -2,25 +2,23 @@
 
 Types used:
 - UniverseElement: A single, unique element of universe. Could be any sort of object
-  that can exist inside a mathematical set, e.g. 1, (1,2), "Cheese".
-- Universe: A collection of elements in some universe. Exact cover is a collection of
-  subsets, where each element of the universe is contained in exactly one subset.
-- SubsetName: Name of the subset, e.g. some number id, like 1 or some string id, like
-  "r3c4#9".
-- Subset: A collection of universe elements. Collection should have a unique name and
-  list of unique elements.
+  that can exist inside a mathematical set, e.g. int 1, tuple (1,2) or str "Cheese".
+- Universe: A collection of universe elements.
+- SubsetName: Name of the subset, should uniquely identify subset from other subsets.
+  Name should be immutable and hence hashable.
+- Subset: A collection of universe elements.
 - SubsetCollection: A collection of subsets.
 - Solution: List of SubsetNames that identify which disjoint subsets were picked to
   solution.
-- ProblemData: Data needed to solve an exact cover problem.
+- ProblemData: Data needed to create an exact cover problem matrix.
 """
 
-from typing import List, Tuple, TypeVar, Union
+from typing import Dict, List, Tuple, TypeVar, Hashable
 
 UniverseElement = TypeVar("UniverseElement")
 Universe = List[UniverseElement]
-SubsetName = Union[int, str]
-Subset = List[UniverseElement]
+SubsetName = TypeVar("SubsetName", bound=Hashable)
+Subset = Dict[SubsetName, List[UniverseElement]]
 SubsetCollection = List[Subset]
 Solution = List[SubsetName]
 ProblemData = Tuple[Universe, SubsetCollection]
