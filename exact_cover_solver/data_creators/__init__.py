f4b8345b6b9b6aacb708b021cb6a568abb36b9fd
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-12-11 21:41:57 +0200

    delete generic creator

--- exact_cover_solver/data_creators/__init__.py
+++ exact_cover_solver/data_creators/__init__.py
@@ -2,6 +2,5 @@
 
 Defines imports for things that can be imported directly from this package.
 """
-from .generic_creator import GenericCreator
 from .pentomino_creator import PentominoCreator
 from .sudoku_creator import SudokuCreator
