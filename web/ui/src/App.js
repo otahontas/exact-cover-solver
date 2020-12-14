c98ce0cf5418cede0ea958f86cb95fa7bad73958
Otto Ahoniemi <otto@ottoahoniemi.fi>
2020-12-14 13:58:38 +0200

    Add view for pentomino selection

--- web/ui/src/App.js
+++ web/ui/src/App.js
@@ -1,40 +1,70 @@
 import React from 'react';
 import {
-  ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  ChakraProvider,
+  Container,
+  HStack,
   theme,
+  Button,
+  Stack,
+  Radio, 
+  RadioGroup,
+  Text
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { useState } from 'react';
+
+const PentominoBoard = ({data}) => {
+  return null;
+}
+
+const SudokuView = () => {
+  return null;
+}
+
+const PentominoView = () => {
+  const [value, setValue] = React.useState("1")
+  return (
+    <Box>
+      <Text>Please select which pentomino board size to use</Text>
+      <RadioGroup onChange={setValue} value={value}>
+        <Stack direction="row">
+          <Radio value="3x20">3x20</Radio>
+          <Radio value="4x15">4x15</Radio>
+          <Radio value="5x12">5x12</Radio>
+          <Radio value="6x10">6x10</Radio>
+        </Stack>
+      </RadioGroup>
+    </Box>
+  )
+}
+
+const Content = ({page}) => {
+  if (page === "pentomino") {
+    return <PentominoView />
+  }
+  if (page === "sudoku") {
+    return <p>Sudoku</p>
+  }
+  return null
+}
 
 function App() {
+  const [page, setPage] = useState("")
+  
+  
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Container>
+        <HStack spacing={8}>
+          <Button colorScheme="teal" variant="outline" onClick={() => setPage("pentomino")}>
+            Pentomino
+          </Button>
+          <Button colorScheme="teal" variant="outline" onClick={() => setPage("sudoku")}>
+            Sudoku
+          </Button>
+        </HStack>
+        <Content page={page} />
+      </Container>
     </ChakraProvider>
   );
 }
